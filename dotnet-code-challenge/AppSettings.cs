ec09dc640d16aca4504bc36f51bf5629aa058f63
bhavesh narola <bhavesh@abacus.co>
2019-12-09 23:16:36 +1100

    code

---
+++ dotnet-code-challenge/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dotnet_code_challenge
+{
+    public class AppSettings
+    {
+        public List<DataSource> DataSources { get; set; }
+
+        public DataSource this[string name]
+        {
+            get { return DataSources.Single(x => x.Name == name); }
+        }
+    }
+
+    public class DataSource
+    {
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+    }
+}
---
+++ dotnet-code-challenge/Services/ConfigService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace dotnet_code_challenge
+{
+    public interface IConfigService
+    {
+        AppSettings Settings { get; set; }
+    }
+
+    /// <summary>
+    /// Config Service: Read data file paths from appsettings.json
+    /// </summary>
+    public class ConfigService : IConfigService
+    {
+        public AppSettings Settings { get; set; }
+
+        public ConfigService()
+        {
+            var builder = new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfiguration configuration = builder.Build();
+
+            this.ReadConfig(configuration);
+        }
+
+        private void ReadConfig(IConfiguration configuration)
+        {
+            this.Settings = new AppSettings();
+            configuration.Bind("Settings", this.Settings);
+        }
+
+    }
+}
